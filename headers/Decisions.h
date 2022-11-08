b7255f1154bd4517f1202fac95eab72a50806c8d
Alwaleed Algheraibi <alwaleedalgheraibi@Alwaleeds-MacBook-Pro.local>
2022-11-08 10:49:09 -0500

    Finished Loops.h, Completed Fucntions.h and added Structs.h functionality.

--- headers/Decisions.h
+++ headers/Decisions.h
@@ -1,5 +1,6 @@
 // include pre-processor directive
 #include <iostream>
+#include <iomanip>
 
 #define NUM 34568.80
 
@@ -13,29 +14,34 @@ void decisions1(void){
     // initialize it using constant defined in header file
     float num = NUM;
 
+    // show decimal point and trailing zeros on the output stream object
+    cout << fixed << showpoint;
+    // show three digits to the right of the decimal on the output stream object
+    cout << setprecision(3);
+
     // use if statement
     if (num < 40000.0) {
-        cout << "Num (" << num <<") is less than 40000.0" << endl;
+        cout << "Num " << num << " is less than 40000.0" << endl;
     }
 
     num += num;
 
     // use if-else statement
     if (num < 40000.0) {
-        cout << "Num (" << num << ") is less than 40000.0" << endl;
+        cout << "Num " << num << " is less than 40000.0" << endl;
     } else {
-        cout << "Num (" << num << ") is not less than 40000.0" << endl;
+        cout << "Num " << num << " is not less than 40000.0" << endl;
     }
 
     num = 0.0;
 
     // use if-else-if statement
     if (num == 0.0) {
-        cout << "Num (" << num << ") is equal to 0.0." << endl;
+        cout << "Num " << num << " is equal to 0.0." << endl;
     } else if (num < 40000.0) {
-        cout << "Num (" << num << ") is not less than 40000.0" << endl;
+        cout << "Num " << num << " is not less than 40000.0" << endl;
     } else {
-        cout << "Num (" << num << ") is not less than 40000.0 and is not equal to 0.0" << endl;
+        cout << "Num " << num << " is not less than 40000.0 and is not equal to 0.0" << endl;
     }
 
 }
